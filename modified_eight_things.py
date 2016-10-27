d8c5a8e7c324c2b523281f12c5767ac665775c2b
Andrew Lampinen <lampinen@stanford.edu>
2016-10-26 23:34:43 -0700

    Hierarchical item structure, restructuring training and testing, outputs

--- modified_eight_things.py
+++ modified_eight_things.py
@@ -2,9 +2,13 @@ import tensorflow as tf
 import numpy
 import matplotlib.pyplot as plt
 
+#reproducibility
+numpy.random.seed(1)
+tf.set_random_seed(1)
+
 #set properties of neural net such as size, input, output
 nDomains = 4
-nItemsPerDomain = 8
+nItemsPerDomain = 8 #should be 2k for some k >= 2, and 2(k+1) should not be larger than nSingleOutputSize
 nContextPerDomain = 4
 nSingleOutputSize = 14
 
@@ -13,14 +17,18 @@ nSizeofContextInput = nDomains*nContextPerDomain#size of context input vector
 
 nSizeofInput = (nItemsPerDomain+nContextPerDomain)*nDomains #length of a single input vector
 nSizeofOutput = nSingleOutputSize*nDomains*nContextPerDomain # length of a single output vector
-nNumberOfInputs = nItemsPerDomain*nContextPerDomain*nDomains #number of training examples in dataset
+nNumberOfPossibleInputPatterns = nItemsPerDomain*nContextPerDomain*nDomains #number of training examples in dataset
 
-sizeItemRepLayer = 32
-sizeContextRepLayer = 16
-sizeSecondLayer = 48
+sizeItemRepLayer = 8
+sizeContextRepLayer = 8
+sizeSecondLayer = 32
+
+#training properties
+nEpochs = 200
+eta = 0.001
 
 #construct the input matrix
-InputMatrix = numpy.empty([nNumberOfInputs,nSizeofInput],int)
+InputMatrix = numpy.empty([nNumberOfPossibleInputPatterns,nSizeofInput],int)
 InputCount = 0
 
 A = numpy.zeros((nItemsPerDomain,nDomains),dtype =numpy.int)
@@ -30,9 +38,9 @@ B = numpy.zeros((nContextPerDomain,nDomains),dtype =numpy.int)
 for i in range(nDomains):
 	for j in range(nItemsPerDomain):
 		A[j,i] = 1
+		tempA = A.flatten('F')
 		for k in range(nContextPerDomain):
 			B[k,i] = 1
-			tempA = A.flatten('F')
 			tempB = B.flatten('F')
 			temp = numpy.concatenate((tempA,tempB))
 			InputMatrix[InputCount] = temp
@@ -44,26 +52,64 @@ for i in range(nDomains):
 
 print InputMatrix.shape #each row is an input vector, number of row is number of training example
 
-#construct the output matrix
-OutputMatrix = numpy.empty([nNumberOfInputs,nSizeofOutput],int)
+#plt.imshow(numpy.dot(InputMatrix,InputMatrix.transpose()),cmap='Greys',interpolation='none')
+#plt.show()
+
+
+#construct the output templates
+OutputMatrix = numpy.zeros([nNumberOfPossibleInputPatterns,nSizeofOutput],int)
+OutputMatrix_ContextCollapsed = numpy.empty([nItemsPerDomain*nDomains,nSizeofOutput],int) #Collapsing across context for similarity computation
 OutputCount = 0
 
-C = numpy.zeros((nContextPerDomain*nDomains,nSingleOutputSize), dtype =numpy.int)
-temprand = numpy.random.randint(2,size=(nDomains*nItemsPerDomain,nSingleOutputSize))
+output_template = numpy.empty([nItemsPerDomain*nContextPerDomain,nSingleOutputSize]) #template for what the outputs in each domain and context will look like
+output_similarity_step_size = nSingleOutputSize//(nItemsPerDomain) #How many entries will be made changed in each pair of items within a context
+for ctx_i in xrange(nContextPerDomain):
+    basic_output = numpy.random.permutation(numpy.concatenate((numpy.zeros(nSingleOutputSize//2),numpy.ones(nSingleOutputSize-(nSingleOutputSize//2))))) #Random pattern that will form basis for the similarity
+    locations = numpy.argsort(basic_output)
+    zero_locations = numpy.random.permutation(locations[:nSingleOutputSize//2]) #at each step of similarity will flip one of these and one of the ones
+    one_locations = numpy.random.permutation(locations[nSingleOutputSize//2:])
+    for out_i in xrange(nItemsPerDomain):
+	this_output = basic_output
+	this_output[zero_locations[out_i//2]] = 1
+	this_output[one_locations[out_i//2]] = 0
+	output_template[ctx_i*nItemsPerDomain+out_i] = this_output 
+	
 
+#Fill output matrix
+for dom_i in xrange(nDomains):
+    for item_i in range(nItemsPerDomain):
+	for ctx_i in xrange(nContextPerDomain):
+	    output_offset = dom_i*nContextPerDomain*nSingleOutputSize+ctx_i*nSingleOutputSize
+	    OutputMatrix[dom_i*nContextPerDomain*nItemsPerDomain+item_i*nContextPerDomain+ctx_i,output_offset:output_offset+nSingleOutputSize] = output_template[ctx_i*nItemsPerDomain+item_i]
+	    OutputMatrix_ContextCollapsed[dom_i*nItemsPerDomain+item_i,output_offset:output_offset+nSingleOutputSize] = output_template[ctx_i*nItemsPerDomain+item_i]
 
-for a in range(nDomains*nContextPerDomain):
-	for b in range(nItemsPerDomain):
-		C[a] = temprand[(b + (a*nItemsPerDomain))%32] #loop over random matrix again to have same output among domains
+print OutputMatrix.shape #order: in order of input, domain -> item -> context 
+plt.imshow(OutputMatrix,interpolation='none')
+plt.savefig('results/OutputMatrix.png')
+plt.close()
+#plt.show()
+#
+#print OutputMatrix_ContextCollapsed.shape
+#plt.imshow(OutputMatrix_ContextCollapsed,interpolation='none')
+#plt.show()
+#
+#Calculate similarity matrices
+OutputSimilarityMatrix = numpy.dot(OutputMatrix,OutputMatrix.transpose())
+#print OutputSimilarityMatrix.shape
+#
+plt.imshow(OutputSimilarityMatrix,cmap='Greys',interpolation='none')
+plt.savefig('results/OutputSimilarityMatrix.png')
+plt.close()
+#plt.show()
 
-		tempC = C.flatten()
-		OutputMatrix[OutputCount] = tempC
-		OutputCount = OutputCount + 1
-
-		C = numpy.zeros((nContextPerDomain*nDomains,nSingleOutputSize), dtype =numpy.int)
-
-
-print OutputMatrix.shape #order: in order of input, first fill all items in domain #1, context #1 -> fill all items in domain #1, context #2; move to domain #2 once all of domain 1 filled
+#Output similarity matrix context collapsed
+Output_ContextCollapsed_SimilarityMatrix = numpy.dot(OutputMatrix_ContextCollapsed,OutputMatrix_ContextCollapsed.transpose())
+#print Output_ContextCollapsed_SimilarityMatrix.shape
+#
+plt.imshow(Output_ContextCollapsed_SimilarityMatrix,cmap='Greys',interpolation='none')
+plt.savefig('results/Output_ContextCollapsed_SimilarityMatrix.png')
+plt.close()
+#plt.show()
 
 
 #set up network below
@@ -87,29 +133,78 @@ W3 = tf.Variable(tf.random_uniform([nSizeofOutput,sizeSecondLayer],-1,1)) #weigh
 B3 = tf.Variable(tf.random_uniform([nSizeofOutput,1],-1,1)) #bias from context hidden layer1 to layer2
 
 #construct network
-itemhidden = tf.nn.relu6(tf.matmul(W1_item,inputvec_item) + B1_item)
-contexthidden = tf.nn.relu6(tf.matmul(W1_context,inputvec_context) + B1_context)
+itemhidden = tf.nn.sigmoid(tf.matmul(W1_item,inputvec_item) + B1_item)
+contexthidden = tf.nn.sigmoid(tf.matmul(W1_context,inputvec_context) + B1_context)
 
-secondhidden = tf.nn.relu6((tf.matmul(W2_item,itemhidden) + B2_item) + (tf.matmul(W2_context,contexthidden) + B2_context))
+secondhidden = tf.nn.sigmoid((tf.matmul(W2_item,itemhidden) + B2_item) + (tf.matmul(W2_context,contexthidden) + B2_context))
 
 output = tf.nn.sigmoid(tf.matmul(W3, secondhidden) + B3)
 
 error = tf.reduce_sum(tf.square(output - outputvec))
-train = tf.train.GradientDescentOptimizer(0.05).minimize(error)
+train = tf.train.AdamOptimizer(eta).minimize(error)
 
 
 listerror = []
-#run the network
-model = tf.initialize_all_variables()
-with tf.Session() as session:
-	session.run(model)
-	for i in range(10000):
-		_ , outerror = session.run([train, error], feed_dict = {inputvec_item: InputMatrix[i%nNumberOfInputs][0:(nSizeofItemInput)].reshape([nSizeofItemInput,1]), 
-															inputvec_context: InputMatrix[i%nNumberOfInputs][nSizeofItemInput:].reshape([nSizeofContextInput,1]),
-															outputvec: OutputMatrix[i%nNumberOfInputs].reshape([nSizeofOutput,1])})
-		
-		if i%1 == 0:
-			listerror.append(outerror)
 
-plt.plot([numpy.mean(listerror[k-nNumberOfInputs:k]) for k in range(len(listerror))])
+def train_error():
+    train_error = 0.0
+    for i in range(nNumberOfPossibleInputPatterns):
+	    train_error += session.run(error, feed_dict = {inputvec_item: InputMatrix[i][0:(nSizeofItemInput)].reshape([nSizeofItemInput,1]),	
+				inputvec_context: InputMatrix[i][nSizeofItemInput:].reshape([nSizeofContextInput,1]), 
+				outputvec: OutputMatrix[i].reshape([nSizeofOutput,1])})
+    return train_error/nNumberOfPossibleInputPatterns 
+
+def get_network_outputs():
+    network_outputs = numpy.zeros([nNumberOfPossibleInputPatterns,nSizeofOutput])
+    for i in range(nNumberOfPossibleInputPatterns):
+	    network_outputs[i,:] = session.run(output, feed_dict = {inputvec_item: InputMatrix[i][0:(nSizeofItemInput)].reshape([nSizeofItemInput,1]),	
+				inputvec_context: InputMatrix[i][nSizeofItemInput:].reshape([nSizeofContextInput,1]), 
+				outputvec: OutputMatrix[i].reshape([nSizeofOutput,1])}).flatten()
+    return network_outputs
+
+
+def get_item_reps():
+    item_reps = numpy.zeros([nNumberOfPossibleInputPatterns,sizeItemRepLayer])
+    for i in range(nNumberOfPossibleInputPatterns):
+	    item_reps[i,:] = session.run(itemhidden, feed_dict = {inputvec_item: InputMatrix[i][0:(nSizeofItemInput)].reshape([nSizeofItemInput,1]),	
+				inputvec_context: InputMatrix[i][nSizeofItemInput:].reshape([nSizeofContextInput,1]), 
+				outputvec: OutputMatrix[i].reshape([nSizeofOutput,1])}).flatten()
+    return item_reps
+
+def log_images(epoch=0):
+	network_outputs = get_network_outputs() 
+	
+	plt.imshow(numpy.dot(network_outputs,OutputMatrix.transpose()),cmap='Greys',interpolation='none')
+	plt.savefig('results/epoch_%i_networks_output_vs_output.png' %epoch)
+	plt.close()
+	
+	item_reps = get_item_reps() 
+	item_rep_norms = numpy.linalg.norm(item_reps,axis=1)
+	item_reps = item_reps / item_rep_norms[:,numpy.newaxis]
+	plt.imshow(numpy.dot(item_reps,item_reps.transpose()),cmap='Greys',interpolation='none') #cosine distance
+	plt.savefig('results/epoch_%i_item_rep_similarity.png' %epoch)
+	plt.close()
+
+#run the network
+init = tf.initialize_all_variables()
+with tf.Session() as session:
+	session.run(init)
+	print "Initial training MSE: ",train_error()
+	listerror.append(train_error())
+	log_images(0)
+	for epoch in xrange(1,nEpochs+1):
+	    this_order = numpy.random.permutation(nNumberOfPossibleInputPatterns) #Present examples in a random order each time
+	    for i in this_order:
+		session.run([train, error], feed_dict = {inputvec_item: InputMatrix[i%nNumberOfPossibleInputPatterns][0:(nSizeofItemInput)].reshape([nSizeofItemInput,1]), 
+															inputvec_context: InputMatrix[i%nNumberOfPossibleInputPatterns][nSizeofItemInput:].reshape([nSizeofContextInput,1]),
+															outputvec: OutputMatrix[i%nNumberOfPossibleInputPatterns].reshape([nSizeofOutput,1])})
+		
+	    listerror.append(train_error())
+	    if epoch % 10 == 0:
+		print "On epoch %i, training MSE: %f" %(epoch,listerror[-1])
+		i = nNumberOfPossibleInputPatterns-1
+	    if epoch % 20 == 0:
+		log_images(epoch)
+
+plt.plot(range(nEpochs+1),listerror)
 plt.show()
